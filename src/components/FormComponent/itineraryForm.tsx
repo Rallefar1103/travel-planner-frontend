46a83de2713670af19bba718d8bf710ffafb69bf
Rallefar1103 <rasmus@turftank.com>
2024-01-12 17:11:32 -0800

    finalizes the form styling

--- src/components/FormComponent/itineraryForm.tsx
+++ src/components/FormComponent/itineraryForm.tsx
@@ -141,192 +141,194 @@ const ItineraryForm: React.FC<ItineraryFormProps> = ({
 
   return (
     <section id="itineraryForm">
-      <div className="left-section">
-        <p className="form-intro">Start by filling out the form! 👉</p>
-        <p className="form-dest-info">
-          <span className="bold">First step</span> is to fill in the essentials
-          of what city you want to visit, how many hours you expect to spend in
-          that city and the budget for your stay.
-        </p>
+      <div className="form-content">
+        <div className="left-section">
+          <p className="form-intro">Start by filling out the form! 👉</p>
+          <p className="form-dest-info">
+            <span className="bold">First step</span> is to fill in the
+            essentials of what city you want to visit, how many hours you expect
+            to spend in that city and the budget for your stay.
+          </p>
 
-        <p className="form-dining-info">
-          <span className="bold">Next step</span> is to tell us something about
-          where you'd like to dine for your stay. Fill out the type of cuisine
-          you'd like, the type of venue, and lastly the desired price range!
-        </p>
+          <p className="form-dining-info">
+            <span className="bold">Next step</span> is to tell us something
+            about where you'd like to dine for your stay. Fill out the type of
+            cuisine you'd like, the type of venue, and lastly the desired price
+            range!
+          </p>
 
-        <p className="form-attractions-info">
-          <span className="bold">Last step</span> is to elaborate on what kind
-          of attractions you'd like to spend your time exploring at your
-          destination. Fill out the type of attraction you wish to visit, and
-          the price range.
-        </p>
+          <p className="form-attractions-info">
+            <span className="bold">Last step</span> is to elaborate on what kind
+            of attractions you'd like to spend your time exploring at your
+            destination. Fill out the type of attraction you wish to visit, and
+            the price range.
+          </p>
 
-        <p className="form-attractions">Attractions preferences</p>
+          <p className="form-attractions">Attractions preferences</p>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="attraction-type">
-            {" "}
-            Type{" "}
-          </label>
-          <select
-            className="form-input"
-            name="userPreferences.attractionOptions.type"
-            value={formState.userPreferences.attractionOptions.type}
-            multiple={false}
-            onChange={handleChange}
-            required={true}
-          >
-            <option value=""> Select Type </option>
-            <option value="art">Art 🖼️ </option>
-            <option value="outdoor">Outdoor 🏞️ </option>
-            <option value="monuments">Monuments 🗽 </option>
-            <option value="religion">Religion 🕌 </option>
-            <option value="sports">Sports 🏟️ </option>
-            <option value="city">City 🌆 </option>
-          </select>
+          <div className="form-group">
+            <label className="form-label" htmlFor="attraction-type">
+              {" "}
+              Type{" "}
+            </label>
+            <select
+              className="form-input"
+              name="userPreferences.attractionOptions.type"
+              value={formState.userPreferences.attractionOptions.type}
+              multiple={false}
+              onChange={handleChange}
+              required={true}
+            >
+              <option value=""> Select Type </option>
+              <option value="art">Art 🖼️ </option>
+              <option value="outdoor">Outdoor 🏞️ </option>
+              <option value="monuments">Monuments 🗽 </option>
+              <option value="religion">Religion 🕌 </option>
+              <option value="sports">Sports 🏟️ </option>
+              <option value="city">City 🌆 </option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label className="form-label" htmlFor="labelForAttractionPrice">
+              {" "}
+              Price{" "}
+            </label>
+            <select
+              className="form-input"
+              name="userPreferences.attractionOptions.priceRange"
+              value={formState.userPreferences.attractionOptions.priceRange}
+              multiple={false}
+              onChange={handleChange}
+              required={true}
+            >
+              <option value=""> Select Price </option>
+              <option value="budget">$ </option>
+              <option value="midrange">$$</option>
+              <option value="highend">$$$</option>
+            </select>
+          </div>
+
+          {/* Include any logos or additional info here */}
         </div>
+        <div className="right-section">
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label className="form-label" htmlFor="destination">
+                🌎 Destination City
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                name="destination"
+                value={formState.destination}
+                onChange={handleChange}
+                placeholder="Where to?"
+                required={true}
+              />
+            </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="labelForAttractionPrice">
-            {" "}
-            Price{" "}
-          </label>
-          <select
-            className="form-input"
-            name="userPreferences.attractionOptions.priceRange"
-            value={formState.userPreferences.attractionOptions.priceRange}
-            multiple={false}
-            onChange={handleChange}
-            required={true}
-          >
-            <option value=""> Select Price </option>
-            <option value="budget">$ </option>
-            <option value="midrange">$$</option>
-            <option value="highend">$$$</option>
-          </select>
+            <div className="form-group">
+              <label className="form-label" htmlFor="duration">
+                ⏳ Hours at destination
+              </label>
+              <input
+                className="form-input"
+                type="number"
+                name="duration"
+                value={formState.duration}
+                onChange={handleChange}
+                required={true}
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label" htmlFor="budget">
+                💰 Budget for your trip ($)
+              </label>
+              <input
+                className="form-input"
+                type="number"
+                name="budget"
+                value={formState.budget}
+                onChange={handleChange}
+                required={true}
+              />
+            </div>
+
+            <h3 className="form-preferences"> More preferences </h3>
+
+            <div className="diningOptions">
+              <div className="form-group">
+                <label className="form-label" htmlFor="cuisine">
+                  {" "}
+                  Cuisine{" "}
+                </label>
+                <select
+                  className="form-input"
+                  name="userPreferences.diningOptions.cuisine"
+                  value={formState.userPreferences.diningOptions.cuisine}
+                  multiple={false}
+                  onChange={handleChange}
+                  required={true}
+                >
+                  <option value=""> Select Cuisine </option>
+                  <option value="italian">Italian 🍝 </option>
+                  <option value="mexican">Mexican 🌮 </option>
+                  <option value="japanese">Japanese 🍣 </option>
+                  <option value="mediterranean">Mediterranean 🥙 </option>
+                  <option value="american">American 🍔 </option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <label className="form-label" htmlFor="restaurant-type">
+                  {" "}
+                  Type{" "}
+                </label>
+                <select
+                  className="form-input"
+                  name="userPreferences.diningOptions.type"
+                  value={formState.userPreferences.diningOptions.type}
+                  multiple={false}
+                  onChange={handleChange}
+                  required={true}
+                >
+                  <option value=""> Select Type </option>
+                  <option value="restaurant">Restaurant 🍴</option>
+                  <option value="bar">Bar 🍸</option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <label className="form-label" htmlFor="restaurant-price">
+                  {" "}
+                  Price{" "}
+                </label>
+                <select
+                  className="form-input"
+                  name="userPreferences.diningOptions.priceRange"
+                  value={formState.userPreferences.diningOptions.priceRange}
+                  multiple={false}
+                  onChange={handleChange}
+                  required={true}
+                >
+                  <option value=""> Select Price Range </option>
+                  <option value="1">$</option>
+                  <option value="2">$$</option>
+                  <option value="3">$$$</option>
+                  <option value="4">$$$$</option>
+                </select>
+              </div>
+            </div>
+          </form>
         </div>
-
-        {/* Include any logos or additional info here */}
       </div>
-      <div className="right-section">
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label className="form-label" htmlFor="destination">
-              🌎 Destination City
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              name="destination"
-              value={formState.destination}
-              onChange={handleChange}
-              placeholder="Where to?"
-              required={true}
-            />
-          </div>
-
-          <div className="form-group">
-            <label className="form-label" htmlFor="duration">
-              ⏳ Hours at destination
-            </label>
-            <input
-              className="form-input"
-              type="number"
-              name="duration"
-              value={formState.duration}
-              onChange={handleChange}
-              required={true}
-            />
-          </div>
-
-          <div className="form-group">
-            <label className="form-label" htmlFor="budget">
-              💰 Budget for your trip ($)
-            </label>
-            <input
-              className="form-input"
-              type="number"
-              name="budget"
-              value={formState.budget}
-              onChange={handleChange}
-              required={true}
-            />
-          </div>
-
-          <h3 className="form-preferences"> More preferences </h3>
-
-          <div className="diningOptions">
-            <div className="form-group">
-              <label className="form-label" htmlFor="cuisine">
-                {" "}
-                Cuisine{" "}
-              </label>
-              <select
-                className="form-input"
-                name="userPreferences.diningOptions.cuisine"
-                value={formState.userPreferences.diningOptions.cuisine}
-                multiple={false}
-                onChange={handleChange}
-                required={true}
-              >
-                <option value=""> Select Cuisine </option>
-                <option value="italian">Italian 🍝 </option>
-                <option value="mexican">Mexican 🌮 </option>
-                <option value="japanese">Japanese 🍣 </option>
-                <option value="mediterranean">Mediterranean 🥙 </option>
-                <option value="american">American 🍔 </option>
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label className="form-label" htmlFor="restaurant-type">
-                {" "}
-                Type{" "}
-              </label>
-              <select
-                className="form-input"
-                name="userPreferences.diningOptions.type"
-                value={formState.userPreferences.diningOptions.type}
-                multiple={false}
-                onChange={handleChange}
-                required={true}
-              >
-                <option value=""> Select Type </option>
-                <option value="restaurant">Restaurant 🍴</option>
-                <option value="bar">Bar 🍸</option>
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label className="form-label" htmlFor="restaurant-price">
-                {" "}
-                Price{" "}
-              </label>
-              <select
-                className="form-input"
-                name="userPreferences.diningOptions.priceRange"
-                value={formState.userPreferences.diningOptions.priceRange}
-                multiple={false}
-                onChange={handleChange}
-                required={true}
-              >
-                <option value=""> Select Price Range </option>
-                <option value="1">$</option>
-                <option value="2">$$</option>
-                <option value="3">$$$</option>
-                <option value="4">$$$$</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="submit-area">
-            <button type="submit" onClick={handleSubmit} disabled={loading}>
-              {loading ? "Creating your itinerary..." : "Create Itinerary"}
-            </button>
-            {error && <p className="error-message">Error: {error.message}</p>}
-          </div>
-        </form>
+      <div className="submit-area">
+        <button type="submit" onClick={handleSubmit} disabled={loading}>
+          {loading ? "Creating your itinerary..." : "Create Itinerary"}
+        </button>
+        {error && <p className="error-message">Error: {error.message}</p>}
       </div>
     </section>
   );
